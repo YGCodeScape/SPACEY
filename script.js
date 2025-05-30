251b54e13a0b69e4bf70cbb26e9cb348bb1c980b
yashG <yrg.creation.ai@gmail.com>
2025-05-30 22:57:53 +0530

    GSAP added

--- script.js
+++ script.js
@@ -21,3 +21,45 @@ document.addEventListener('click', function (e) {
 
 //GSAP animation start here.
 
+var tl = gsap.timeline();
+
+function breakTheText() {
+    var logoText = document.querySelector(".word-mark");
+    var logoTextContent = logoText.textContent;
+    var splittedText = logoTextContent.split("");
+    var clutter = "";
+
+    splittedText.forEach(function(letter) {
+        clutter += `<span>${letter}</span>`;
+    });
+
+    logoText.innerHTML = clutter;
+}
+breakTheText(); // call the function to work it.
+
+
+tl.from(".word-mark span", {  // animation for move that split text.
+  y: -70, 
+  opacity: 0,
+  duration: 1,
+  delay:0.2,
+  stagger:0.15,
+})
+
+tl.from(".search-d", {
+    top: -150,
+    duration: 0.3,
+})
+tl.from(".n-f-ul li", {
+    y: -150,
+    duration: 0.3,
+    stagger: 0.2,
+    opacity: 0,
+})
+
+tl.from(".con-d span", {
+    y: -150,
+    duration: 0.3,
+    opacity: 0,
+    stagger: 0.2,
+})
