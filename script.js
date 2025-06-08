ec0b9d69be3a7df25b580e6466999a01f896782d
yashG <yrg.creation.ai@gmail.com>
2025-06-08 21:48:34 +0530

    planet card added

--- script.js
+++ script.js
@@ -19,74 +19,72 @@ document.addEventListener('click', function (e) {
     }
 });
 
-//GSAP animation start here.
+//--------------- GSAP animation start here.
 
-// var tl = gsap.timeline();
+var tl = gsap.timeline();
 
-// function breakTheText() {
-//     var logoText = document.querySelector(".word-mark");
-//     var logoTextContent = logoText.textContent;
-//     var splittedText = logoTextContent.split("");
-//     var clutter = "";
+// land page gsap animation
+tl.from( ".land-text-hold h3",
+    { scale: -0, opacity: 0 },
+    { scale: 1, opacity: 1, duration: 0.5, ease: "power2.out" }
+);
+tl.from( ".land-text-hold h1",
+    { scale: -0, opacity: 0 },
+    { scale: 1, opacity: 1, duration: 0.6, ease: "power2.out" }
+);
 
-//     splittedText.forEach(function(letter) {
-//         clutter += `<span>${letter}</span>`;
-//     });
-
-//     logoText.innerHTML = clutter;
-// }
-// breakTheText(); // call the function to work it.
-
-
-// tl.from(".word-mark span", {  // animation for move that split text.
-//   y: -70, 
-//   opacity: 0,
-//   duration: 1,
-//   delay:0.2,
-//   stagger:0.15,
-// })
-
-// tl.from(".search-d", {
-//     top: -150,
-//     duration: 0.3,
-// })
-// tl.from(".n-f-ul li", {
-//     y: -150,
-//     duration: 0.3,
-//     stagger: 0.2,
-//     opacity: 0,
-// })
-
-// tl.from(".con-d span", {
-//     y: -150,
-//     duration: 0.3,
-//     opacity: 0,
-//     stagger: 0.2,
-// })
+tl.to(".obj-hold img", {
+    scale: 1,
+    opacity: 1,
+    duration: 1.5,
+    ease: "back.out(1.7)"
+})
+tl.to(".land-text", {
+    scale: 1,
+    opacity: 1,
+    duration: 1,
+    stagger: 1,
+    ease: "back.out(1.7)"
+ });
 
 
-// tl.from(
-//     ".land-text h3",
-//     { scale: -0, opacity: 0 },
-//     { scale: 1, opacity: 1, duration: 0.5, ease: "power2.out" }
-// );
-// tl.from(
-//     ".land-text h1",
-//     { scale: -0, opacity: 0 },
-//     { scale: 1, opacity: 1, duration: 0.6, ease: "power2.out" }
-// );
+function breakTheText() {
+    var logoText = document.querySelector(".word-mark");
+    var logoTextContent = logoText.textContent;
+    var splittedText = logoTextContent.split("");
+    var clutter = "";
 
-// tl.to(".land-text", {
-//     scale: 1,
-//     opacity: 1,
-//     duration: 1,
-//     stagger: 1,
-//     ease: "back.out(1.7)"
-//  });
+    splittedText.forEach(function(letter) {
+        clutter += `<span>${letter}</span>`;
+    });
 
-// tl.to(".obj-hold img", {
-//     scale: 1,
-//     opacity: 1,
-//     duration: 1.5,
-//     ease: "back.out(1.7)"
-// })
+    logoText.innerHTML = clutter;
+}
+breakTheText(); // call the function to work it.
+
+// navigation gsap
+tl.from(".word-mark span", {  // animation for move that split text.
+  y: -70, 
+  opacity: 0,
+  duration: 1,
+  delay:0.2,
+  stagger:0.15,
+})
+
+tl.from(".search-d", {
+    top: -150,
+    duration: 0.3,
+})
+tl.from(".n-f-ul li", {
+    y: -150,
+    duration: 0.3,
+    stagger: 0.2,
+    opacity: 0,
+})
+
+tl.from(".con-d span", {
+    y: -150,
+    duration: 0.3,
+    opacity: 0,
+    stagger: 0.2,
+})
